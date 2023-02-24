2e797d284087a86b0fbea4276006e5ddb1482e22
David Fang <fanghuateng0621@gmail.com>
2023-02-24 15:59:16 +0800

    complete home and article page with graphql

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import { useQuery, gql } from '@apollo/client'
+
+const ARTICLE = gql`
+  query GetArticle($id: ID!) {
+    article(id: $id) {
+      data {
+        id
+        attributes {
+          title
+          body
+          rating
+        }
+      }
+    }
+  }
+`
 
 export default function Article() {
     const { id } = useParams()
-    const { loading, error, data } = useFetch('http://localhost:1337/api/articles/' + id)
+    const { loading, error, data } = useQuery(ARTICLE, {
+        variables: { id: id }
+    })
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error to fetch the data.</p>
 
-    console.log(data)
-
-  return (
-    <div key={data.id} className="article-card">
-        <div className="rating">{data.attributes.rating}</div>
-        <h2>{data.attributes.title}</h2>
-        <small>Caregory</small>
-        <p>{data.attributes.body}</p>
-    </div>
-  )
+    return (
+      <div className="article-card">
+          <div className="rating">{data.article.data.attributes.rating}</div>
+          <h2>{data.article.data.attributes.title}</h2>
+          <small>Caregory</small>
+          <p>{data.article.data.attributes.body}</p>
+      </div>
+    )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const ARTICLES = gql`
+  query {
+    articles {
+      data {
+        id
+        attributes {
+          title
+          rating
+          body
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
-    const { loading, error, data } = useFetch('http://localhost:1337/api/articles')
+  const { loading, error, data } = useQuery(ARTICLES);
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error to fetch the data.</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error to fetch the data.</p>;
 
-    return (
-        <div>
-            {data.map(article => (
-                <div key={article.id} className="article-card">
-                    <div className="rating">{article.attributes.rating}</div>
-                    <h2>{article.attributes.title}</h2>
-                    <small>Caregory</small>
-                    <p>{article.attributes.body.substring(0, 200)}...</p>
-                    <Link to={`/details/${article.id}`}>Read more</Link>
-                </div>
-            ))}
+  return (
+    <div>
+      {data.articles.data.map((article) => (
+        <div key={article.id} className="article-card">
+          <div className="rating">{article.attributes.rating}</div>
+          <h2>{article.attributes.title}</h2>
+          <small>Caregory</small>
+          <p>{article.attributes.body.substring(0, 200)}...</p>
+          <Link to={`/details/${article.id}`}>Read more</Link>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
