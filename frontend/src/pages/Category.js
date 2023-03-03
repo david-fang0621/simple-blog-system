1cd4ff42085b7724cb2f8f506eee7bd0725b32fc
David Fang <fanghuateng0621@gmail.com>
2023-03-03 19:05:00 +0800

    remove Link import

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import ArticleCard from "../components/ArticleCard";
 
 const CATEGORY = gql`
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 import ArticleCard from "../components/ArticleCard";
 
